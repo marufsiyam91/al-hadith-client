14a2213dbb225d81e51d153dba8000cf4641dd0b
marufsiyam91 <marufsiyam9123@gmail.com>
2024-05-02 22:53:06 +0600

    navbar issue fixed

--- src/App.jsx
+++ src/App.jsx
@@ -13,14 +13,14 @@ function App() {
         <div className="xl:w-[96px]">
           <Navbar />
         </div>
-        <div className="w-full h-[calc(100vh-120px)] xl:h-[calc(100vh-80px)] bg-[#F2F4F6] xl:rounded-tl-3xl p-3 xl:p-5 pr-4 flex gap-6 justify-between">
-          <div className="2xl:block hidden">
+        <div className="w-full h-[calc(100vh-120px)] xl:h-[calc(100vh-80px)] bg-[#F2F4F6] xl:rounded-tl-3xl flex  gap-6 justify-between">
+          <div className="2xl:block hidden 2xl:m-5 2xl:mr-0">
             <Bookbar />
           </div>
-          <div className="w-full">
+          <div className="w-full mt-5 4xl:mr-0 xl:mr-5 mr-3 2xl:ml-0 xl:ml-5 ml-3">
             <HadithCard />
           </div>
-          <div className="4xl:block hidden">
+          <div className="4xl:block hidden 4xl:m-5 4xl:ml-0">
             <Settingsbar />
           </div>
         </div>
