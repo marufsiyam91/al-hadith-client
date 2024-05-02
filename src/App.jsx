51c68bd5a58b8c6005838963c9884733f0082135
marufsiyam91 <marufsiyam9123@gmail.com>
2024-05-02 22:17:18 +0600

    some bug fixed

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
         <div className="xl:w-[96px]">
           <Navbar />
         </div>
-        <div className="w-full h-[calc(100vh-80px)] bg-[#F2F4F6] xl:rounded-tl-3xl p-5 pr-4 flex gap-6 justify-between">
+        <div className="w-full h-[calc(100vh-120px)] xl:h-[calc(100vh-80px)] bg-[#F2F4F6] xl:rounded-tl-3xl p-3 xl:p-5 pr-4 flex gap-6 justify-between">
           <div className="2xl:block hidden">
             <Bookbar />
           </div>
